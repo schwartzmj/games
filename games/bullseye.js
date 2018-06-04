e7e90e0eebf0577737a4fc744339891803f5943c
schwartzmj <admin@schwartzmj.com>
2018-06-03 21:57:36 -0500

    end of tonight

--- games/bullseye.js
+++ games/bullseye.js
@@ -2,10 +2,13 @@
 
 
 
-bullseyes = [];
+bullseyeArr = [];
+
+arr = [1,2];
+
 
 hitScore = 0;
-
+missScore = 0;
 function getCanvasWidth() {
 	return $("#gameArea").width();
 }
@@ -26,15 +29,34 @@ function generateCoordinate() {
 
 function generateBullseye() {
 	$("#gameArea").html("<div class='bullseye' style='margin-left:" + generateCoordinate()[0] + "px;margin-top:" + generateCoordinate()[1] + "px',></div>");
-}
+};
+
+
+// function addBullseyeToArray() {
+// 	// $("#gameArea").html("<div class='bullseye' style='margin-left:" + generateCoordinate()[0] + "px;margin-top:" + generateCoordinate()[1] + "px',></div>");
+// 	var bullseye = "<div class='bullseye' style='margin-left:" + generateCoordinate()[0] + "px;margin-top:" + generateCoordinate()[1] + "px',></div>";
+// 	bullseyeArr.push(bullseye);
+// 	console.log(bullseyeArr);
+// }
 
 setInterval(generateBullseye, 1000);
 
 $("#gameArea").on("click",".bullseye",function(){
 	hitScore += 1;
 	$("#score").text(hitScore);
-	console.log(hitScore);
-	console.log("clicked circle");
+	// console.log(hitScore);
+	// console.log("clicked circle");
 	$(this).css("backgroundColor","blue");
 	$(this).addClass("bullseyeHit");
 });
+
+// $("#gameArea").on("click",function(){
+// 	if ($(this).attr('id') != "bullseye") {
+
+// 		missScore += 1;
+// 		$("#misses").text(missScore);
+// 		// console.log("miss score:" + missScore);
+// 		// console.log("clicked");
+// 		$(this).addClass("miss");
+// 	};
+// });
