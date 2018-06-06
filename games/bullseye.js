b66172d81589daf6dee6ed3b4e0b8194b4d7d9b8
schwartzmj <admin@schwartzmj.com>
2018-06-06 00:29:31 -0500

    tough bullseye night

--- games/bullseye.js
+++ games/bullseye.js
@@ -3,32 +3,97 @@
 anyClick = 0;
 hitScore = 0;
 missScore = 0;
+
+survivalScore = 10;
+
 var hitSound = new Howl({
   		src: ['piston-1.mp3']
 });
 
+//all game modes = defaultMode, survivalMode
+var gameMode = "survival";
+
+init();
+
+function init() {
+	if (gameMode === "survival") {
+		survivalSetUp();
+	}
+};
+
+function survivalSetUp() {
+	$("#gameArea").append("<div id='survivalCircle'></div>");
+};
+
+
 function getCanvasWidth() {
 	return $("#gameArea").width();
-}
+};
 
 function getCanvasHeight() {
 	return $("#gameArea").height();
-}
+};
 
 function coordinateMax() {
 	return [getCanvasWidth(), getCanvasHeight()];
-}
+};
+
+function getBullseyeWidth() {
+	return parseFloat($(".bullseye").css("width"));
+
+};
+
+function getBullseyeHeight() {
+	return parseFloat($(".bullseye").css("height"));
+};
+
 
 function generateCoordinate() {
 	var x = Math.random() * getCanvasWidth();
 	var y = Math.random() * getCanvasHeight();
-	return [x, y];
-}
-
-function generateBullseye() {
-	$("#gameArea").append("<div class='bullseye' style='margin-left:" + generateCoordinate()[0] + "px;margin-top:" + generateCoordinate()[1] + "px',></div>");
+	return [calculateTargetX(x), calculateTargetY(y)];
 };
 
+function calculateTargetX(x) {
+	if (x > (getCanvasWidth() - getBullseyeWidth())) {
+		x -= getBullseyeWidth();
+		return x;	
+	} 
+	else {
+		return x;
+	}
+};
+
+function calculateTargetY(y) {
+	if (y > (getCanvasHeight() - getBullseyeHeight())) {
+		y -= getBullseyeHeight();
+		return y;	
+	} else {
+		return y;
+	}
+};
+
+function generateBullseye() {
+	xCoordinate = generateCoordinate()[0];
+	yCoordinate = generateCoordinate()[1];
+	if (gameMode === "survival") {
+		//********************************************************************
+		//USE THESE NUMBERS TO ADJUST SURVIVAL MODE "NO SPAWN ZONE" IN MIDDLE
+		//********************************************************************
+		if (((xCoordinate < (getCanvasWidth() * 0.3)) || (xCoordinate > (getCanvasWidth() * 0.65))) 
+			|| ((yCoordinate < (getCanvasHeight() * 0.3)) || (yCoordinate > (getCanvasHeight() * 0.65)))) {
+			drawBullseye(xCoordinate, yCoordinate);
+		} else {
+			generateBullseye();	
+		}
+	} else {
+		drawBullseye();
+	}
+};
+
+function drawBullseye(xCoordinate, yCoordinate) {
+	$("#gameArea").append("<div class='bullseye' style='margin-left:" + xCoordinate + "px;margin-top:" + yCoordinate + "px',></div>");
+}
 
 // function addBullseyeToArray() {
 // 	// $("#gameArea").html("<div class='bullseye' style='margin-left:" + generateCoordinate()[0] + "px;margin-top:" + generateCoordinate()[1] + "px',></div>");
@@ -48,6 +113,7 @@ $("#gameArea").on("click",".bullseye",function(ev){
 	// $(this).addClass("bullseyeHit");
 	$(this).hide();
 	hitSound.play();
+	//stop propagation doenst do anything(?)
 	ev.stopPropagation();
 });
 
@@ -63,3 +129,36 @@ $("#gameArea").click(function(){
 		// console.log("clicked");
 		$("body").prepend("<div id='missArea'style='display:block;'></div>");
 });
+
+// survivalCounter = 5000;
+
+// mouseLeaveInterval = 5000;
+
+
+// $("#survivalCircle").one("mouseleave", function(){
+
+// 	console.log(mouseLeaveInterval);
+
+// 		var interval = setInterval(function(){
+// 			mouseLeaveInterval -= 1000;
+// 			// survivalCounter -= 1;
+// 			// console.log(survivalCounter);
+// 		}, 1000);
+// 		$("#survivalCircle").fadeOut(mouseLeaveInterval);
+// 		console.log('LEAVE');
+// 		console.log(interval);
+// });
+
+// function mouseEnter(interval){ $("#survivalCircle").on("mouseenter", function(){
+// 		$("#survivalCircle").stop();
+// 		clearInterval(interval);
+// 				console.log(interval + "enter interval log");
+// 		console.log('ENTER');
+// 	});
+// };
+
+//
+// **** USE "ONE" INSTEAD OF "ON" FOR ONLY ONE EVENT GENERATION ON MOUSE LEAVE
+//
+
+
