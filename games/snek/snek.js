b30671b580a9918e009969bbaf3cc41f9e8c0c94
schwartzmj <admin@schwartzmj.com>
2018-06-09 06:20:05 -0500

    6:19 AM end of snek day 1

--- games/snek/snek.js
+++ games/snek/snek.js
@@ -9,6 +9,7 @@ var snekBodyArr = [];
 // these get unshifted into b/c body generates at [0,1] each time
 var appleCoords = [];
 var currentAppleCoords = [];
+var snekMovements = [];
 
 var gameArea = document.querySelector("#gameArea");
 var snekHead = document.querySelector("#snekHead");
@@ -17,10 +18,20 @@ var apple = document.querySelector(".apple");
 var score = document.querySelector("#score");
 var snekBody = document.querySelector(".snekBody");
 
+function captureSnekMovements(time){
+    var xStart = getCoords(snekHead)[0];
+    var xEnd = getCoords(snekHead)[1];
+    var yStart = getCoords(snekHead)[2];
+    var yEnd = getCoords(snekHead)[3];
+    time = [xStart, xEnd, yStart, yEnd];
+    snekMovements.unshift(time);
+};
+
 init();
 
 function clock(){
     time += tickRate / 1000;
+    captureSnekMovements(time);
     move(snekHead);
     if (generateBodyCheck === true) {
         generateBody();
@@ -57,10 +68,10 @@ function getCoords(elem) {
 //*Snake only - Do math and update direction before drawing)
 //
 function move(elem) {
-    newGridRowStart = getCoords(elem)[0];
-    newGridRowEnd = getCoords(elem)[1];
-    newGridColStart = getCoords(elem)[2];
-    newGridColEnd = getCoords(elem)[3];
+    newGridRowStart = (getCoords(elem)[0]);
+    newGridRowEnd = (getCoords(elem)[1]);
+    newGridColStart = (getCoords(elem)[2]);
+    newGridColEnd = (getCoords(elem)[3]);
     if (direction === "up") {
         newGridRowStart -= 1;
         newGridRowEnd -= 1;
@@ -75,11 +86,28 @@ function move(elem) {
         newGridColEnd += 1;
     }
     drawElem(elem);
-        if ((getCoords(elem)[0] === 0) || (getCoords(elem)[0] === 11) || 
-            (getCoords(elem)[2] === 0) || (getCoords(elem)[2] === 11)) {
+        if ((newGridRowStart === 0) || (newGridRowStart === 11) ||
+            (newGridColStart === 0) || (newGridColStart === 11)) {
             endGame("You went out of bounds!");
         }
+    //
+    //tail code
+    //
+    $(".snekBody").remove();
+    for (i = 0; i <= snekLength; i++) {
+        gameArea.insertAdjacentHTML('beforeend', "<div class='snekBody' id='snekBody" + snekLength + "'  style='grid-row: " + snekMovements[i][0] +
+            "/" + snekMovements[i][1] + "; grid-column: " + snekMovements[i][2] + "/" + snekMovements[i][3] + ";'></div>");
+        snekBodyArr.push(document.querySelector("#snekBody" + snekLength));
+    }
 };
+    // for (i = 0; i <= snekLength; i++) {
+    //     function removeElement(elementId) {
+    //         // Removes an element from the document
+    //         var element = document.getElementById(elementId);
+    //         element.parentNode.removeChild(element);
+    //     }
+
+  
 //
 //Draw element on grid
 //
@@ -92,10 +120,16 @@ function generateApple(){
     if (window.getComputedStyle(apple).display === "none") {
     var appleX = Math.floor(Math.random() * 10) + 1;
     var appleY = Math.floor(Math.random() * 10) + 1;
+    //re-generate if Apple lands on snake body (add head too later?)
+    for (i = 0; i <= snekLength; i++) {
+       if ((appleX === snekMovements[i][0]) && (appleY === snekMovements[i][2])) {
+           generateApple();
+       }
+    }
     apple.style.gridRow = appleX + "/" + (appleX + 1);
     apple.style.gridColumn = appleY + "/" + (appleY + 1);
     apple.style.display = "block";
-    currentAppleCoords.shift(appleX, appleY);
+    currentAppleCoords.unshift(appleX, appleY);
     }
 };
 
@@ -114,18 +148,27 @@ function checkCollision(){
         console.log(snekLength);
         console.log("Snek ate an apple.");
     }
+    for (i = 0; i <= snekLength; i++) {
+       if ((snekRowStart === snekMovements[i][0]) && (snekColStart === snekMovements[i][2])) {
+        endGame("That's a long johnson");
+       }
+        
+    }
+    
 }
 
-function generateBody() {
-    var bodyRowStart = appleCoords[0];
-    var bodyRowEnd = (bodyRowStart + 1)
-    var bodyColStart = appleCoords[1];
-    var bodyColEnd = (bodyColStart + 1);
-    gameArea.insertAdjacentHTML('beforeend', "<div class='snekBody' id='snekBody" + snekLength + "' style='grid-row: " + bodyRowStart +
-        "/" + bodyRowEnd + "; grid-column: " + bodyColStart + "/" + bodyColEnd + ";'></div>");
-    snekBodyArr.push(document.querySelector("#snekBody" + snekLength));
-    console.log("#snekBody" + snekLength + " added to snekBodyArr array: " + snekBodyArr[snekLength]);
-};
+//========Does this code not matter anymore with the tail code "for" loops in "move()"?
+//========
+function generateBody() {}
+//     // var bodyRowStart = appleCoords[0];
+//     // var bodyRowEnd = (bodyRowStart + 1)
+//     // var bodyColStart = appleCoords[1];
+//     // var bodyColEnd = (bodyColStart + 1);
+//     // gameArea.insertAdjacentHTML('beforeend', "<div class='snekBody' id='snekBody" + snekLength + "' style='grid-row: " + bodyRowStart +
+//     //     "/" + bodyRowEnd + "; grid-column: " + bodyColStart + "/" + bodyColEnd + ";'></div>");
+//     // snekBodyArr.push(document.querySelector("#snekBody" + snekLength));
+//     // console.log("#snekBody" + snekLength + " added to snekBodyArr array: " + snekBodyArr[snekLength]);
+// };
 
 document.addEventListener('keypress', function(){
     var keyName = event.key;
